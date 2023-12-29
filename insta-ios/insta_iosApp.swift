375a15839b6c417f198bb1cc27b1dbfff4049a27
Pavel Naumov <malinatrashh@gmail.com>
2023-12-29 19:37:10 +0800

    Initial Commit

---
+++ insta-ios/insta_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  insta_iosApp.swift
+//  insta-ios
+//
+//  Created by Pavel Naumov on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct insta_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
