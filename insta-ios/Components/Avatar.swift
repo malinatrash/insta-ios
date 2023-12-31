35939039476d267deabec7b71d4bf975dfca39e2
Pavel Naumov <malinatrashh@gmail.com>
2023-12-31 15:19:15 +0800

    Profile page

---
+++ insta-ios/Components/Avatar.swift
@@ -0,0 +1,18 @@
+//
+//  Avatar.swift
+//  insta-ios
+//
+//  Created by Pavel Naumov on 31.12.2023.
+//
+
+import SwiftUI
+
+struct Avatar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Avatar()
+}
